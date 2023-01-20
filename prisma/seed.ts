4191691cc8a5ff216abb85f85207bd9b6784e080
Frank Condezo <frankcondezo@gmail.com>
2023-01-20 16:13:52 -0500

    refactor some things

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,14 +4,14 @@ import bcrypt from "bcryptjs";
 const prisma = new PrismaClient();
 
 async function seed() {
-  const email = "rachel@remix.run";
+  const email = "spectre@gmail.com";
 
   // cleanup the existing database
   await prisma.user.delete({ where: { email } }).catch(() => {
     // no worries if it doesn't exist yet
   });
 
-  const hashedPassword = await bcrypt.hash("racheliscool", 10);
+  const hashedPassword = await bcrypt.hash("lucas123", 10);
 
   const user = await prisma.user.create({
     data: {
