12713d5a36e9c380935accb240ec24ab28e55e8a
Frank Condezo <frankcondezo@gmail.com>
2023-03-05 20:28:59 -0500

    update seed users

--- prisma/seed-players.ts
+++ prisma/seed-players.ts
@@ -1,61 +1,62 @@
-export const textPlayers = `Frank		5
-Piero go		6
-Lucho go		6
-Bruce go		6
-Jheyson go		7
-Houston go		6
-Luis Daniel		6
-Juanecito		7
-Ronaldo el bicho		5
-Ronaldo A		5
-Ivan		4
-Gorpa		4
-Kenyi		4
-Ario		7
-Juan Masgo		9
-Sandrito		2
-Trejo		6
-Cato		8
-Angel		5
-Alexis		5
-Terry		4
-JP		5
-Jhoan		7
-Mikhail		8
-Luis R		6
-Luigi alva		6
-Erick Jr		8
-Vladi		5
-Erick Valdivia		4
-Alpaquitay		7
-Alanya		4
-Yonil		6
-Ricardo		7
-Martin		4
-Davis		5
-Gala		3
-Zalasar		7
-Luigi Rocha		4
-Diego Vega		4
-Jhil		7
-Francis z		5
+export const textPlayers = `Angel M		5
+Anghelo		6
+Luis simon		5
+Josue		4
+Cantaro		5
+Jara		6
+Amigo de jhoan		5
+Fisher		5
+Salas		5
+Mori		3
+Pavel		2
+Jheampol		7
+Amigo de Ronaldo A.		5
+Loloy		5
 Jhonatan		3
 Gregory		4
 Paulo T.		3
-Amigo de Ronaldo A.		5
-Loloy		5
-Pavel		2
-Jheampol		7
-Mori		3
-Salas		5
-Fisher		5
-Amigo de jhoan		5
-Jara		6
-Josue		4
-Cantaro		5
-Luis simon		5
-Anghelo		6
-Angel M		5`;
+Jhil		7
+Francis z		5
+Luigi Rocha		4
+Diego Vega		4
+Gala		3
+Zalasar		7
+Ricardo		7
+Martin		4
+Davis		5
+Alanya		4
+Yonil		6
+Erick Valdivia		4
+Alpaquitay		7
+Erick Jr		8
+Vladi		5
+Luigi alva		6
+Mikhail		8
+Luis R		6
+Jhoan		7
+Terry		4
+JP		5
+Alexis		5
+Cato		8
+Angel		5
+Trejo		6
+Sandrito		2
+Juan Masgo		9
+Ario		7
+Kenyi		4
+Gorpa		4
+Ivan		4
+Ronaldo A		5
+Ronaldo el bicho		5
+Juanecito		7
+Luis Daniel		6
+Houston go		6
+Jheyson go		7
+Bruce go		6
+Lucho go		6
+Piero go		6
+Frank		5
+Lucas		5`;
 
 export const players = textPlayers.split("\n").map((player) => {
   const [name, number] = player.split("\t\t");
